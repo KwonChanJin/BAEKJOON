1648bde4fe8e36239a1903b2f6a09675c22a3768
KwonChanJin <kchanjini@gmail.com>
2021-07-18 22:00:03 +0900

    step01

---
+++ step01/곱셈.py
@@ -0,0 +1,13 @@
+one = int(input(""))
+two = int(input(""))
+num1 = two % 10
+num2 = two // 10 % 10
+num3 = two // 100
+three = one * num1
+four = one * num2
+five = one * num3
+six = three + four*10 + five*100
+print(three)
+print(four)
+print(five)
+print(six)
